0d8ea6f59605424a3e8be901cc6f05f2e0feb834
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2015-10-23 14:49:16 +0900

    locale-config.js | ko-KR 추가

--- app/common/configs/locale-config.js
+++ app/common/configs/locale-config.js
@@ -1,13 +1,13 @@
 module.exports = ['$translateProvider', function ($translateProvider) {
     $translateProvider.useSanitizeValueStrategy('escaped');
 
-    $translateProvider.translations('en', require('../locales/en.json'));
+    $translateProvider.translations('en', require('../locales/en.json')).translations('ko-KR', require('../locales/ko-KR.json'));
 
     $translateProvider.useStaticFilesLoader({
         prefix: 'locales/',
         suffix: '.json'
     });
 
-    $translateProvider.preferredLanguage('en');
+    $translateProvider.preferredLanguage('ko-KR');
     $translateProvider.fallbackLanguage('en');
 }];
